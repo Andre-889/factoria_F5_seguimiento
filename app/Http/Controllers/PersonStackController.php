a00e8bd9aa69ece85414248cf0aae1c87bd34377
BeatrizCano <beatrizcafernandez@gmail.com>
2023-10-02 12:59:31 +0200

    create_table_and_controller_and_model_person_stack

---
+++ app/Http/Controllers/PersonStackController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PersonStackController extends Controller
+{
+    //
+}
---
+++ app/Models/PersonStack.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PersonStack extends Model
+{
+    use HasFactory;
+}
