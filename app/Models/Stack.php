a4c08e9992448146771311b1e097f6a8c224c282
NINETTHE <raissa.escobar1408@gmail.com>
2023-10-06 12:28:59 +0200

    Added bolongsto to model stack

--- app/Models/Stack.php
+++ app/Models/Stack.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 
@@ -25,7 +26,6 @@ class Stack extends Model
     public function skills(): BelongsTo
     {
         return $this->belongsTo(Skill::class);
-                   
     }
     
 }
