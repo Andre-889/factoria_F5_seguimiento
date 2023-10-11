d10b1591263a531208b1834cd88e0c8e96260f0d
BeatrizCano <beatrizcafernandez@gmail.com>
2023-10-11 10:43:43 +0200

    create_DatabaseSeeder_code_and_DatabaseServiceProvider_file_and_changes_in_phpunit.xml_and_.env.testing

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FactoryServiceProvider::class,
+        App\Providers\DatabaseServiceProvider::class,
     ])->toArray(),
 
     /*
