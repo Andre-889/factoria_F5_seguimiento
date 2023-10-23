554c72ac3a00948bab634b04876347f23ca8c92d
NINETTHE <raissa.escobar1408@gmail.com>
2023-10-23 15:36:41 +0200

    EvalutionRequest

--- app/Http/Requests/EvaluationRequest.php
+++ app/Http/Requests/EvaluationRequest.php
@@ -11,7 +11,7 @@ class EvaluationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,11 +22,24 @@ class EvaluationRequest extends FormRequest
     public function rules(): array
     {
         return [
-                'date' => 'required|date',
-                'type' => 'required|enum',
+                'date' => 'date',
+                'type' => 'required',
                 'person_id' => 'required|integer',
-                'mean' => 'nullable|float',
+                'mean' => 'nullable|floatmax:2',
                 'user_id' => 'required|integer'
         ];
+    }
+    
+        public function messages(): array
+    {
+        return [
+            'date.date' => 'Enter date format year-month-day',
+            'type.required' => 'Type is required and accepts EVALUACION, CO-EVALUACION, AUTO-EVALUACION',
+            'person_id.required' => 'Person id is required',
+            'person_id.integer' => 'Person id accepts only integers',
+            'mean.floatmax' => 'Mean accepts numbers with decimals, two decimals max',
+            'user_id.required' => 'User id is required',
+            'user_id.integer' => 'User id accepts only inteegers',
+            ];
     }
 }
